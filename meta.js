f180d12ab576a68e78b3cee8dc27a76fa2753e7f
linwei9208 <linwei9208@zto.com>
2020-04-16 14:46:49 +0800

    init

--- meta.js
+++ meta.js
@@ -1,4 +1,17 @@
 module.exports = {
+  helpers: {
+    if_or(v1, v2, options) {
+
+      if (v1 || v2) {
+        return options.fn(this)
+      }
+
+      return options.inverse(this)
+    },
+    template_version() {
+      return templateVersion
+    },
+  },
   prompts: {
     name: {
       when: 'isNotTest',
